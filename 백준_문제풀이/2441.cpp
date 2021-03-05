cb7acf7174a128ac4d5683080c0c620f865b4286
doomdabo <72394295+doomdabo@users.noreply.github.com>
2021-03-05 23:26:29 +0900

    Create 2441.cpp

---
+++ 백준_문제풀이/2441.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+/*
+별 찍기 - 4
+첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+*/
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=n-i;k>0;k--)
+            cout<<"*";
+        cout<<"\n";
+    }
+
+    return 0;
+}
