5ec29b6521ae425b47dcbca8479dba91aa986bba
doomdabo <72394295+doomdabo@users.noreply.github.com>
2021-03-05 23:30:22 +0900

    Create 10171.cpp

---
+++ 백준_문제풀이/10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"\\    /\\"<<"\n";
+    cout<<" )  ( \')"<<"\n";
+    cout<<"(  /  )"<<"\n";
+    cout<<" \\(__)|"<<"\n";
+    return 0;
+}
+//주의:\을 사용하기 위해서는 \\과 같이 두번 역슬래시 입력해야함
+//주의:작은따옴표 사용하려면 앞에\붙이기
