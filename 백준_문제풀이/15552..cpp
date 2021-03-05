1eac95c3a71dbb43481042a551cc5aabf915b7e0
doomdabo <72394295+doomdabo@users.noreply.github.com>
2021-03-05 23:31:57 +0900

    Create 15552..cpp

---
+++ 백준_문제풀이/15552..cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+/*
+빠른 A+B
+본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다. 입출력 방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다.
+
+C++을 사용하고 있고 cin/cout을 사용하고자 한다면, 
+cin.tie(NULL)과 sync_with_stdio(false)를 둘 다 적용해 주고, endl 대신 개행문자(\n)를 쓰자. 단, 이렇게 하면 더 이상 scanf/printf/puts/getchar/putchar 등 
+C의 입출력 방식을 사용하면 안 된다.
+*/
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+    
+
+    return 0;
+}
